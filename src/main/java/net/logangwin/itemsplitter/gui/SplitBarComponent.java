ac7d198432e2af61cfcad03aebea0116f86f3a9f
logers234 <logers520@gmail.com>
2026-07-19 03:18:07 -0500

    Added slider thumb to split bar; Rewrote text rendering to center when jumping between double and single digits.

--- src/main/java/net/logangwin/itemsplitter/gui/SplitBarComponent.java
+++ src/main/java/net/logangwin/itemsplitter/gui/SplitBarComponent.java
@@ -15,15 +15,16 @@ public class SplitBarComponent implements TooltipComponent {
     private final int barWidth = 50;
     private final int barPadding = 3;
     private final float textScale = 0.75F;
+    private final int maxTextWidth;
 
     SplitBarComponent (double progress, TextRenderer textRenderer) {
         this.progress = progress;
 
         // Calculate maximum text width
         String temp = "00";
-        int textWidth = textRenderer.getWidth(temp);
-        textWidth = (int) (textWidth * textScale);
-
+        int textWidth = (int) (textRenderer.getWidth(temp) * textScale);
+        this.maxTextWidth = textWidth;
+        
         // Calculate total tooltip width
         this.width = this.barWidth + (this.barPadding * 2) + (textWidth * 2);
     }
@@ -48,20 +49,37 @@ public class SplitBarComponent implements TooltipComponent {
 
         int currentItems = Math.round((float) (SplitScreenLogic.getMaxSplit() * progress));
 
+
+        // Calculate the static X positions where the text bounding boxes should start/end
+        int leftTextAnchor = x + center - (barWidth / 2) - barPadding;
+        int rightTextAnchor = x + center + (barWidth / 2) + barPadding;
+
         // Left side text (Display number of items that will remain in inventory stack)
-        String text = String.valueOf(SplitScreenLogic.getMaxSplit() - currentItems);
-        int textWidth = (int) (textRenderer.getWidth(text) * textScale);
-        int textX = x + center - (barWidth / 2) - barPadding - textWidth;
-        drawSplitText(textRenderer, context, text, textX, y);
+        String leftText = String.valueOf(SplitScreenLogic.getMaxSplit() - currentItems);
+        int leftWidth = (int) (textRenderer.getWidth(leftText) * textScale);
+
+        // Standardize the box boundary.
+        int leftBoxX = leftTextAnchor - maxTextWidth;
+        int leftTextX = leftBoxX + ((maxTextWidth - leftWidth) / 2);
+
+        drawSplitText(textRenderer, context, leftText, leftTextX, y);
 
         // Right side text (Display number of items that will be picked up by cursor)
-        text = String.valueOf(currentItems);
-        textX = x + center + (barWidth / 2) + barPadding;
-        drawSplitText(textRenderer, context, text, textX, y);
+        String rightText = String.valueOf(currentItems);
+        int rightWidth = (int) (textRenderer.getWidth(rightText) * textScale);
+
+        // Left-align the box first, then shift inward by half the unused space to center it.
+        int rightTextX = rightTextAnchor + ((maxTextWidth - rightWidth) / 2);
+
+        drawSplitText(textRenderer, context, rightText, rightTextX, y);
     }
 
     @Unique
     private void drawSplitBar(DrawContext context, TextRenderer textRenderer, int x, int y) {
+        // Values
+        int thumbWidth = 2;
+        int thumbHeight = 4;
+
         // Calculate offsets
         int offset = (int) Math.floor(heightPadding / 2.0F);
         int center = (int) Math.floor(this.getWidth(textRenderer) / 2.0F);
@@ -76,6 +94,13 @@ public class SplitBarComponent implements TooltipComponent {
 
         // Draw split bar
         context.fill(barX, y + offset, barX + progressWidth, y + offset + height, 0xFFFFFFFF);
+
+        // Draw split bar thumb
+        context.fill(barX + progressWidth - (thumbWidth / 2),
+                     y + offset + (height / 2) + (thumbHeight / 2),
+                     barX + progressWidth + (thumbWidth / 2),
+                     y + offset + (height / 2) - (thumbHeight / 2),
+                    0xFFFFFFFF);
     }
 
     @Unique
